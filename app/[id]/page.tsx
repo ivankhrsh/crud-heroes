dfeed74d2ed2a9355d14d0a0cb5954a2af63a7a6
Ivan Khoroshylov <host513@gmail.com>
2023-09-15 22:42:40 +0300

    fix

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -20,7 +20,7 @@ const HeroPage = async ({ params }: { params: { id: string } }) => {
             <Link
               href="/"
               className="group mt-20 sm:mt-0 rounded-full flex w-fit space-x-5 mb-5 bg-white/30 shadow-sm ring-1 ring-gray-900/5 text-gray-600 text-sm font-medium px-10 py-2 hover:shadow-lg active:shadow-sm transition-all"
-              >
+            >
               <ExpandingArrow/>
               <p>Back</p>
             </Link>
